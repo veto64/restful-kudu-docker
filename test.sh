777bb2f165133076243cac9567bd449721bac2d9
Your Name <veto@myridia.com>
2018-09-05 22:48:05 +0700

    use home directory for test

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-docker run --network=kudu-docker_queen  --rm -v "/Users/veto/webs/kudu-docker:/opt/test"  veto64/kudu-docker /usr/bin/python /opt/test/main.py
+#!/bin/bash
+docker run --network=kudu-docker_queen  --rm -v "`pwd`:/opt/test"  veto64/kudu-docker /usr/bin/python /opt/test/main.py
