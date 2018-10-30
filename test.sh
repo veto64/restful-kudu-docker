71550a7f02c04d6aeaafc9336c3c0dfc75b0334e
veto <veto@myridia.com>
2018-10-30 03:07:36 +0000

    new docker can use dash

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --network=kududocker_queen  --rm -v "`pwd`:/opt/test"  veto64/kudu-docker /usr/bin/python /opt/test/main.py
+docker run --network=kudu-docker_queen  --rm -v "`pwd`:/opt/test"  veto64/kudu-docker /usr/bin/python /opt/test/main.py
